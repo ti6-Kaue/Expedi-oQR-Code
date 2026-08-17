cc53e2d8595345aee204732e00afbce36f928420
ti6-Kaue <ti6@talmax.com.br>
2026-08-17 11:11:38 -0300

    Refactor...

---
+++ android/app/src/main/kotlin/com/example/layout_base/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.layout_base
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
-// Entrada principal do app Flutter.
-// Observacao: aqui so iniciamos o aplicativo; a tela e o tema ficam em lib/src/.
-// Comunica-se com: src/app/qr_datamatrix_app.dart.
 import 'package:flutter/material.dart';
 
-import 'src/app/qr_datamatrix_app.dart';
+import 'src/app/app.dart';
 
 void main() {
-  // runApp entrega ao Flutter o primeiro componente da interface.
-  runApp(const QrDataMatrixApp());
+  runApp(const LayoutBaseApp());
 }
